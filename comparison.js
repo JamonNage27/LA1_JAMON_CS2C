a134553c2f94ca159e5a794df0e2224c006ba860
JamonNage27 <nagelygansaenjamon@gmail.com>
2024-09-23 18:56:27 +0800

    Create comparison.js

---
+++ comparison.js
@@ -0,0 +1,12 @@
+// comparison activity 
+var firstFavNumb = 07;
+var secondFavNumb = 27;
+
+console.log(firstFavNumb > secondFavNumb);
+console.log(firstFavNumb < secondFavNumb);
+console.log(firstFavNumb >= secondFavNumb);
+console.log(firstFavNumb <= secondFavNumb);
+console.log(firstFavNumb === secondFavNumb);
+console.log(firstFavNumb == secondFavNumb);
+console.log(firstFavNumb !== secondFavNumb);
+console.log(firstFavNumb != secondFavNumb);

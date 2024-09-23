100394c9f079601b76be07d414239200fc130f40
JamonNage27 <nagelygansaenjamon@gmail.com>
2024-09-23 18:47:59 +0800

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,8 @@
+// Variables Activity 
+var WhatDoYouWannaBecomeInYourLife = " Programmer";
+var gender = "Female";
+var Twitter handle = "DigimarkDiva";
+
+console.log(WhatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);

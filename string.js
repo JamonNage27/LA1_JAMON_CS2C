4134e8b8da2f81fe72c3463f7d8881ff04b336a6
JamonNage27 <nagelygansaenjamon@gmail.com>
2024-09-23 19:04:36 +0800

    Create string.js

---
+++ string.js
@@ -0,0 +1,9 @@
+//strings
+let favActorFirstName = " Marian";
+let favActorLastName =  "Rivera";
+let fullName = favActorFirstName+""+ favActorLastName;
+let changeCaps = fullName.toUpperCase();
+let message = "My favorite actor is";
+let message2 = "  his best show is Marimar";
+
+console.log(message+fullName.toUpperCase() + message2);
